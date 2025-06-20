07e86bc6383fdd6f0eb7e2bd94acde9639ba8cc7
jasilnmyg <mygloyalty3@gmail.com>
2025-06-20 14:48:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -936,8 +936,11 @@ with tab3:
 
                 mapped_keywords = []
                 for sku_key, keywords in sku_category_mapping.items():
-                    if sku_key in retailer_sku:
-                        mapped_keywords = [kw.lower() for kw in keywords]
+                    for keyword in keywords:
+                        if keyword.lower() in retailer_sku.lower():
+                            mapped_keywords = [kw.lower() for kw in keywords]
+                            break
+                    if mapped_keywords:
                         break
 
                 filtered = user_products[user_products['Category'].str.lower().isin(mapped_keywords)]
